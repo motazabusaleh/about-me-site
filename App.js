fe5a6a897c9419d6d7492479f9816f1a769e069d
motazabusaleh <motaz989898@gmail.com>
2020-12-30 14:01:47 +0200

    last-update

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 'use strict'; // use strict mode to prevent doing mistakes 
-
+var wrongCounter = 0;
+var rightCounter = 0;
 var username = prompt("What is your name?")
 alert(
     "Hello Mr. " + username
@@ -10,8 +11,10 @@ console.log(userInput);
 
 if (userInput.toLowerCase() === 'yes') {
     alert('I am glad to that you said yes :)');
+    rightCounter++;
 } else {
     alert('That made me sad :(');
+    wrongCounter++;
 }
 
 var userInput = prompt("would you like to know where did i studied ? (yes , no)");
@@ -19,26 +22,32 @@ console.log(userInput);
 
 if (userInput.toLowerCase() === 'yes') {
     alert('Lovely :)');
+    rightCounter++;
 } else {
     alert(' CRY :(');
+    wrongCounter++;
 }
 
 var userInput = prompt("would you like to know what is my goals? (yes , no)");
 console.log(userInput)
 if (userInput.toLowerCase() === 'yes') {
     alert("Thats lovely ill show you some of them")
+    rightCounter++;
 }
 else {
     alert("Your lose anyway :(")
+    wrongCounter++;
 }
 
 var userInput = prompt("would you like to know how many years did my education last? (yes , no)");
 console.log(userInput)
 if (userInput.toLowerCase() === 'yes') {
     alert("Thats lovely ill tell you")
+    rightCounter++;
 }
 else {
     alert("Idont care about you anyway :(")
+    wrongCounter++;
 }
 
 var guess = prompt("Guess a number: ");
@@ -51,14 +60,16 @@ while (numGuesses < allowedGuess) {
     console.log(allowedGuess);
     if (number == guess) {
         alert("you guessed corecct");
+        rightCounter++;
+        break;
     }
-    
+
     else if (number < guess) {
 
         guess = prompt("number is high! Keep guessing!");
         numGuesses++;
     } else if (number > guess) {
-        
+
         guess = prompt("number is low! Keep guessing!");
         numGuesses++;
 
@@ -86,10 +97,16 @@ while (numGuesses < allowedGuess) {
         guess = prompt("number is low! Keep guessing!");
         numGuesses++;
 
-    }else if (number == guess) {
+    } else if (number === guess) {
         alert("you guessed corecct");
-        }
+        rightCounter++;
+        break;
+        
+    }
 }
+
+alert(wrongCounter);
+alert(rightCounter);
 
 
 
